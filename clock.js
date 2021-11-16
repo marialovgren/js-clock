453623d294fe6760157cc465d4605e0f1c65d6b2
marialovgren <marialovgren86@hotmail.com>
2021-11-16 15:16:30 +0100

    Shows the time live

--- clock.js
+++ clock.js
@@ -1,13 +1,15 @@
-console.log(1);
-console.log(2);
-console.log(3);
+/*
+* Clock 
+*/
 
+
+/*
 // setTimeout är inte BLOCKERANDE utan den startas och så körs resterande kod
 setTimeout( () => {
     console.log("Timeout")
 }, 5000);
 
-/*
+
 let tick = 0;
 setInterval( () => {
     tick++;
@@ -16,9 +18,71 @@ setInterval( () => {
 
 console.log("ticker started");
 
-
-/*
 setInterval( () => {
     console.log("Intervall")
 }, 1000);
+*/
+
+/*
+const now = new Date(); //ger oss tid och datum när koden körs
+
+now.getFullYear();
+now.getDate();
+now.getMonth();
+now.getMonth() + 1;
+now.getHours();
+now.getMinutes();
+now.getSeconds();
+
+// stänga intervall
+clearInterval(); // inom parantesen skickas namnet på funktionen med som ska avslutas
+*/
+
+const hourEl = document.querySelector('#hour');
+const minuteEl = document.querySelector('#minute');
+const secondEl = document.querySelector('#second');
+
+const now = () => {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    // gör att det läggs till en nolla om talet är mellen 0-9
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+
+    hourEl.innerText = `${hour}`;
+    minuteEl.innerText = `${minute}`;
+    secondEl.innerText = `${second}`;     
+};
+
+setInterval("now()", 1000);
+
+/*
+let tick = 0;
+setInterval( () => {
+    tick++;
+    console.log("second", tick);
+}, 1000);
+
+
+/*
+// setTimeout är inte BLOCKERANDE utan den startas och så körs resterande kod
+setTimeout( () => {
+    console.log("Timeout")
+}, 5000);
+
+
+console.log("ticker started");
+
+setInterval( () => {
+    console.log("Intervall")
+}, 1000);
+
+
+// stänga intervall
+clearInterval(); // inom parantesen skickas namnet på funktionen med som ska avslutas
+
 */
